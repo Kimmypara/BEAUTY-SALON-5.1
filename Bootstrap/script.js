7e540eece78c722471890b7768d133fdaa1c3ff2
Kimmypara <107879145+Kimmypara@users.noreply.github.com>
2024-12-28 09:34:19 +0100

    js

--- Bootstrap/script.js
+++ Bootstrap/script.js
@@ -1,19 +1,11 @@
-// get elements by their ID
-const searchInput = document.getElementById('input');
-const searchButton = document.getElementById('search');
+//let iconBag = document.getElementById("#shoppingBag");
+//let closeBag = document.getElementById("#close-Bag");
+//let body = document.querySelector('body');
+//console.log
 
-// add event listener to the button
-searchButton.addEventListener('click', function() {
-  // get the search input value
-  const searchTerm = input.value;
-  // do something with the search term (e.g. redirect to a search results page)
-  console.log(`Searching for "${searchTerm}"...`);
-});
 
-// add event listener to the input (if you want to trigger the search on Enter keypress)
-input.addEventListener('keyup', function(event) {
-  if (event.keyCode === 13) {
-    // simulate a button click to trigger the search
-    search.click();
-  }
-});
+
+//function openIconBag(){
+  //document.getElementById("shoppingBag").classList.toggle("showBag");
+  //document.getElementById("closeBag").classList.toggle("hideBag");
+//}
