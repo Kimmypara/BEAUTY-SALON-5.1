801de100243c5d1636eddf6233a0dc6cd3e3f51e
Kimmypara <107879145+Kimmypara@users.noreply.github.com>
2025-01-02 20:30:57 +0100

    js
    Form

--- Bootstrap/script.js
+++ Bootstrap/script.js
@@ -1,18 +1,75 @@
-console.log
-
 function validate(){
   let errors = " ";
-
+//Get the value of the user input and copy it into variable
   let firstName = document.getElementById("firstName").value;
-  //Condition
-  if (firstName == " "){
-    errors = errors + "<li>Please write your first name</li>";
-    document.getElementById("firstName").classList.add("inputError");
+//Condition 
+  if(firstName == ""){  
+errors = errors +"<li>Please enter First Name</li>";
+document.getElementById("firstName").classList.add("inputError");
   }
   else{
-    document.getElementById("firstName").classList.remove("inputError");
+      document.getElementById("firstName").classList.remove("inputError");
   }
+
+  let lastName = document.getElementById("lastName").value;
+  //Condition 
+    if(lastName == ""){  
+  errors = errors +"<li>Please enter Last Name</li>";
+  document.getElementById("lastName").classList.add("inputError");
+    }
+    else{
+        document.getElementById("lastName").classList.remove("inputError");
+    }
+
+    let contactNumber = document.getElementById("contactNumber").value;
+    //Condition 
+      if(contactNumber == ""){  
+    errors = errors +"<li>Please enter Telephone number</li>";
+    document.getElementById("contactNumber").classList.add("inputError");
+      }
+      else{
+          document.getElementById("contactNumber").classList.remove("inputError");
+      }
+
+      
+        var subject = document.getElementById("subject");
+        if (subject.value == "") {
+            //If the "Please Select" option is selected display error.
+            errors = errors +"<li>Please selet a subject</li>";
+            document.getElementById("subject").classList.add("inputError");
+        }
+        else{
+          document.getElementById("subject").classList.remove("inputError");
+      }
+    
+
+    let email = document.getElementById("email").value;
+
+    if (email == ""){
+        errors = errors + "<li>Please enter your email address</li>";
+    }
+
+    
+    let message = document.getElementById("message").value;
+
+    if (message == ""){
+        errors = errors + "<li>Please write your message</li>";
+    }
+
+
+  //If errors are not empty, meaning if we have errors, add them to the HTML list and show it
+  if(errors != ""){
+    document.getElementById("errorList").innerHTML = errors;
+    document.getElementById("errorList").classList.remove("hidden");
+    return false;
 }
+//if user has no errors, hide the errorList again
+else{
+    document.getElementById("errorList").classList.add("hidden");
+    return true;
+}
+}
+
 
 
 
