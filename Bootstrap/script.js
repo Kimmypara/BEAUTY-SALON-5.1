187fbc3a39da37a2ea1154f593e489cf77fe5c09
Kimmypara <107879145+Kimmypara@users.noreply.github.com>
2025-01-02 18:41:38 +0100

    input errors

--- Bootstrap/script.js
+++ Bootstrap/script.js
@@ -1,3 +1,25 @@
+console.log
+
+function validate(){
+  let errors = " ";
+
+  let firstName = document.getElementById("firstName").value;
+  //Condition
+  if (firstName == " "){
+    errors = errors + "<li>Please write your first name</li>";
+    document.getElementById("firstName").classList.add("inputError");
+  }
+  else{
+    document.getElementById("firstName").classList.remove("inputError");
+  }
+}
+
+
+
+
+
+
+
 //let iconBag = document.getElementById("#shoppingBag");
 //let closeBag = document.getElementById("#close-Bag");
 //let body = document.querySelector('body');
